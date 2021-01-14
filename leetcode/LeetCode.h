a4b0ef9d544ddbdcfaff9750578294cb6ac080fc
Lakshmi S. Patel <er.lspatel@gmail.com>
2021-01-14 07:47:10 +0530

    Conan setup steps

---
+++ leetcode/LeetCode.h
@@ -0,0 +1,18 @@
+//
+// Created by Lakshmi S Patel on 13/01/21.
+//
+
+#ifndef CPPCOURSE_LEETCODE_H
+#define CPPCOURSE_LEETCODE_H
+#include <string>
+
+namespace leetcode {
+
+    class LeetCode {
+    public:
+        static std::string welcomeMsg() { return "Welcome to leet-code challenges"; }
+    };
+
+};
+#endif //CPPCOURSE_LEETCODE_H
+
--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include "leetcode/LeetCode.h"
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    leetcode::LeetCode::welcomeMsg();
     return 0;
 }
 
