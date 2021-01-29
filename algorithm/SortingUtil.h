948d9f063838b5b5a04a38cce21f17a45367a3f5
Lakshmi S. Patel <er.lspatel@gmail.com>
2021-01-30 03:58:01 +0530

    Three way partition

--- algorithm/SortingUtil.h
+++ algorithm/SortingUtil.h
@@ -11,8 +11,11 @@ namespace sortutil {
 
     class SortingUtil {
     public:
+         void swap(int *a, int *b){ int temp = *a; *a = *b; *b = temp; }
          void insertionSort(int *arr,int size);
          int partition(int *arr, int low, int high);
+         int hoarePartition(int *arr, int low, int high);
+         pair<int, int> threeWayPartition(int *arr,int pivot, int low, int high);
     };
 
 };
@@ -30,7 +33,7 @@ void sortutil::SortingUtil::insertionSort(int *arr, int size) {
         int key = arr[i];
         int j = i-1;
         while( j >= 0 && key < arr[j]){
-            swap(arr[j], arr[j+1]);
+            swap(&arr[j], &arr[j+1]);
             j--;
         }
         arr[j+1] = key;
@@ -56,20 +59,83 @@ int sortutil::SortingUtil::partition(int *arr, int low, int high){
      *
      * */
 
-    int pivot = arr[size-1];
+    int pivot = arr[high];
     int trackingIndex = low-1;
 
     for( int i = low; i <= high-1; i++){
 
         if( arr[i] < pivot){
             trackingIndex++;
-            swap(arr[i], arr[trackingIndex]);
+            swap(&arr[i], &arr[trackingIndex]);
         }
-        swap(arr[i], arr[++trackingIndex]);
-        return trackingIndex;
+    }
+
+    swap(&arr[high], &arr[++trackingIndex]);
+    return trackingIndex;
+
+}
+
+
+int sortutil::SortingUtil::hoarePartition(int *arr, int low, int high){
+
+    int pivot = arr[low];
+    int leftIndex = low-1;
+    int rightIndex = high+1;
+
+    while(true){
+
+        do{
+            leftIndex++;
+        }while( arr[leftIndex] < pivot);
+
+        do{
+            rightIndex--;
+        }while(arr[rightIndex] > pivot);
+
+        if( rightIndex <= leftIndex ){
+           return rightIndex;
+        }
+
+        swap(&arr[leftIndex], &arr[rightIndex]);
 
     }
 
+}
+
+pair<int, int> sortutil::SortingUtil::threeWayPartition(int *arr,int pivot, int low, int high){
+
+    /* l, r, u
+     * l : arr[ low, l ] for all elements are less than pivot.
+     * r : arr[ l, r ] for all elements are equal to pivot.
+     *     arr[ r, u] all elements are unexamined.
+     * u : arr[ u, high ] for all elements are greater than pivot.
+     *
+     * */
+
+    if( pivot == INT_MIN)
+        pivot = arr[low];
+
+    int l = low;
+    int r = low;
+    int u = high;
+
+    while( r <= u){
+        if( arr[r] < pivot){
+            swap(&arr[l], &arr[r]);
+            l++;
+            r++;
+        }else if( arr[r] > pivot){
+            swap(&arr[r], &arr[u]);
+            u--;
+        }else{
+            r++;
+        }
+    }
+
+    std::pair<int,int> p;
+    p.first = l;
+    p.second = r;
+    return p;
 }
 
 
